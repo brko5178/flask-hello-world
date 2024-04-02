828c5042754f02ff4dd30e45a43ee778a5ae67d9
brko5178 <brko5178@colorado.edu>
2024-04-02 17:47:55 +0000

    Added test route and added requirement

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+import psycopg2
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Brandon Kohrt in 3308'
+
+@app.route('/db_test')
+def test():
+    conn = psycopg2.connect("postgres://lab10brko_user:IguC1w19ZFjTTNIUbd06hgxdQKkCnLLn@dpg-co644jv109ks73dnspmg-a/lab10brko")
+    conn.close()
+    return "Database connection successful"
